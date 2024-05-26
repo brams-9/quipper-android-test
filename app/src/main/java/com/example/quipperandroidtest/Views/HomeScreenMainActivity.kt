0201e66d7ef75009145c91adbdfc7dac9be23f9b
brams9 <bramantyarahman@gmail.com>
2024-05-26 09:41:00 +0700

    feat: integrate mainActivity with viewModel

--- app/src/main/java/com/example/quipperandroidtest/Views/HomeScreenMainActivity.kt
+++ app/src/main/java/com/example/quipperandroidtest/Views/HomeScreenMainActivity.kt
@@ -1,15 +1,21 @@
 package com.example.quipperandroidtest
 
 import android.os.Bundle
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.quipperandroidtest.ViewModels.PlaylistViewModel
 import com.example.quipperandroidtest.Views.PlaylistAdapter
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class HomeScreenMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val playlistViewModel: PlaylistViewModel by viewModels()
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
@@ -18,5 +24,9 @@ class HomeScreenMainActivity : AppCompatActivity() {
 
         val adapter = PlaylistAdapter()
         recyclerView.adapter = adapter
+
+        playlistViewModel.videos.observe(this, Observer { videos ->
+            videos?.let { adapter.setVideos(it) }
+        })
     }
 }
