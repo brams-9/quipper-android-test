653b02b17217011ed7fc3e64c7791e56db5e4d27
brams9 <bramantyarahman@gmail.com>
2024-05-26 09:38:23 +0700

    feat: setup playlist view model with liveData and hilt injector

--- app/src/main/java/com/example/quipperandroidtest/ViewModels/HomeScreenViewModel.kt
+++ app/src/main/java/com/example/quipperandroidtest/ViewModels/HomeScreenViewModel.kt
@@ -1,7 +1,17 @@
 package com.example.quipperandroidtest.ViewModels
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.liveData
+import com.example.quipperandroidtest.Repositories.PlaylistDataRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import javax.inject.Inject
 
-class HomeScreenViewModel: ViewModel() {
+@HiltViewModel
+class PlaylistViewModel @Inject constructor(private val repository: PlaylistDataRepository) : ViewModel() {
 
+    val videos = liveData(Dispatchers.IO) {
+        val retrievedVideos = repository.fetchVideos()
+        emit(retrievedVideos)
+    }
 }
