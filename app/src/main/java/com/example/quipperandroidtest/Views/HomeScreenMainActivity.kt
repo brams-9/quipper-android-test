7336a2322f8ff15b1f15449cb154b85dfe4ae750
bram <rahman.bramantya@pinhome.id>
2024-05-26 09:33:43 +0700

    feat: setup video recycler view

--- app/src/main/java/com/example/quipperandroidtest/Views/HomeScreenMainActivity.kt
+++ app/src/main/java/com/example/quipperandroidtest/Views/HomeScreenMainActivity.kt
@@ -2,10 +2,21 @@ package com.example.quipperandroidtest
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.quipperandroidtest.Views.PlaylistAdapter
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HomeScreenMainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val recyclerView: RecyclerView = findViewById(R.id.videoRecyclerView)
+        recyclerView.layoutManager = LinearLayoutManager(this)
+
+        val adapter = PlaylistAdapter()
+        recyclerView.adapter = adapter
     }
 }
